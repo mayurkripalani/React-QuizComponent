c3106734d2dbadd18032c26adbff70158d004010
Mayur Kripalani <mayurkripalani.portfolio@outlook.com>
2023-03-31 11:01:54 +0530

    Changes for Creating a QuizEnd Component2

--- src/Quiz.js
+++ src/Quiz.js
@@ -16,9 +16,8 @@ class Quiz extends Component {
         return (
             <div>
                 <QuizEnd />
-                <QuizQuestion quiz_question = {quizData.quiz_questions[this.state.quiz_position - 1]}/>
                 {
-                    isQuizEnd ? <QuizEnd/> : ''
+                    isQuizEnd ? <QuizEnd/> : <QuizQuestion quiz_question = {quizData.quiz_questions[this.state.quiz_position - 1]}/>
                 }
             </div>
         )
