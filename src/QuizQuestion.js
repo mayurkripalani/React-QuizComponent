381b2773266394534c7dc552516fc59f11b80ca3
Mayur Kripalani <mayurkripalani.portfolio@outlook.com>
2023-03-31 12:21:53 +0530

    Changes for Error Message

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -3,9 +3,19 @@ import QuizQuestionButton from './QuizQuestionButton'
 
 
 class QuizQuestion extends Component {
+    constructor(props) {
+        super (props)
+        this.state = {
+            incorrectAnswer: false
+        }
+    }
     handleClick(buttonText) {
         if(buttonText === this.props.quiz_question.answer) {
+            this.setState({ incorrectAnswer: false })
             this.props.showNextQuestionHandler()
+        }
+        else {
+            this.setState({ incorrectAnswer: true })
         }
     }
     render() {
@@ -23,6 +33,7 @@ class QuizQuestion extends Component {
                 )}
               </ul>
             </section>
+            {this.state.incorrectAnswer ? <p className='error'>Sorry, thas's not right</p> : null}
         </main>
         )
     }
